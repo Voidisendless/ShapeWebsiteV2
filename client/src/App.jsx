522756ee05f408e390cceb95bfcc29a154ce6053
Voidisendless <81834147+Voidisendless@users.noreply.github.com>
2025-05-13 18:36:28 +0000

    updated message mapping

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -144,8 +144,9 @@ function App() {
           minHeight: 0,
         }}>
           {filteredMessages.map((m, i) => {
-            const isSelf = m.sender.toLowerCase() === username.toLowerCase();
+            const isSelf = m.userId === socket.id;
             const isBot = m.bot === true;
+
 
 
             return (
