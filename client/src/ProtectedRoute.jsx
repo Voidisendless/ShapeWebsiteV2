4dbb40cd959a07521d02ac4f8b5c1bc9de65c186
void <bluerazzanana@gmail.com>
2025-05-14 01:16:40 +0100

    updated ProtectedRoute

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -2,8 +2,9 @@ import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem('authToken');
+  const guest = localStorage.getItem('guestName');
 
-  return token ? children : <Navigate to="/login" />;
+  return token || guest ? children : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
